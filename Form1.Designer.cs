f94a0acb9e22c8c17aa2799dda8478408e172d5d
Vladimir Ivanov <terrorv@gmail.com>
2015-04-13 10:02:15 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace SpanishVerbs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(0, 28);
+            this.textBox1.MaxLength = 1000000000;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(872, 253);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(0, 287);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(872, 208);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(0, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(784, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(796, 4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(76, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Alternative";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 507);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpanishVerbs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //if (string.IsNullOrEmpty(textBox1.Text))
+            //    return;
+
+            ////textBox2.Text = TableFrom123Spanish();
+            ////if (string.IsNullOrEmpty(textBox2.Text))
+            ////{
+
+            //textBox2.Text = TableFromBabLa(textBox1.Text);
+            ////}
+        }
+
+        private string TableFromBabLa(string text)
+        {
+            //Regex rxVerbs = new Regex(@"<h5 [^>]*>(Indicativo presente|Indicativo pretérito perfecto compuest|Indicativo pretérito perfecto simple)</h5>(<span [^>]*>([\w/]*)</span>\s*(\w*)(<br>)*)*");
+            Regex rxVerbs = new Regex(@"<h5 [^>]*>(Indicativo presente|Indicativo pretérito perfecto compuesto|Indicativo pretérito perfecto simple)</h5>(<span [^>]*>([\w/]*)</span>\s*([\s\w]*)(<br>)*)*");
+
+            MatchCollection matches = rxVerbs.Matches(text);
+            if (matches.Count == 0)
+            {
+                return string.Empty;
+            }
+
+            string[,] verbGrid = new string[matches.Count + 1, 6];
+            verbGrid[0, 0] = "Yo";
+            verbGrid[0, 1] = "Tu";
+            verbGrid[0, 2] = "El/Ella/Ud";
+            verbGrid[0, 3] = "Nosotros/as";
+            verbGrid[0, 4] = "Vosotros/as";
+            verbGrid[0, 5] = "Ellos/Ellas/Uds";
+
+            for (int i = 0; i < matches.Count; i++)
+            {
+                for (int j = 0; j < matches[0].Groups[4].Captures.Count; j++)
+                {
+                    verbGrid[i + 1, j] = matches[i].Groups[4].Captures[j].Value;
+                }
+            }
+            
+            Regex rxGerunds = new Regex(@"<h5 [^>]*>(Gerundio)</h5>(<span [^>]*>([\w/]*)</span>\s*([\s\w]*)(<br>)*)*");
+            MatchCollection matchesGerund = rxGerunds.Matches(textBox1.Text);
+            
+            return TableFromArray(matches.Count, matchesGerund[0].Groups[4].Value, verbGrid);
+        }
+
+        private string TableFrom123Spanish()
+        {
+            //Regex rx = new Regex(@"<tr>\s*<td [^>]*><a[^>]*>([\w\s-]+)(</a>)*\s*</td>\s*(<td[^>]*>([^<]+)</td>\s*)+</tr>");
+            //Regex rxVerbs = new Regex(@"<tr>\s*<td [^>]*><a[^>]*>(Present|Present Perfect|Imperfect|Preterite)(</a>)*\s*</td>\s*(<td[^>]*>([^<]+)</td>\s*)+</tr>");
+            Regex rxVerbs = new Regex(@"<tr>\s*<td [^>]*><a[^>]*title=" + "\"" + @"(Present Tense Indicative|Present perfect Indicative|Preterite)[^>]*>(Present|Present Perfect|Imperfect|Preterite)(</a>)*\s*</td>\s*(<td[^>]*>([^<]+)</td>\s*)+</tr>");
+            //Regex rxGerunds = new Regex(@"Present Participle.*\s*<td.*conjugation[^>]*>(.*)</td>");
+            Regex rxGerunds = new Regex(@"Present Participle.*\s*(<td.*conjugation english[^>]*>.*</td>)*\s*(<td.*conjugation[^>]*>(.*)</td>)");
+
+            MatchCollection matches = rxVerbs.Matches(textBox1.Text);
+            MatchCollection matchesGerund = rxGerunds.Matches(textBox1.Text);
+            List<string> transponedMatches = new List<string>();
+            //transponedMatches.AddRange(new string[matches.Count]);
+            if (matches.Count == 0)
+            {
+                return string.Empty;
+            }
+
+            string[,] verbGrid = new string[matches.Count + 1, 6];
+            verbGrid[0, 0] = "Yo";
+            verbGrid[0, 1] = "Tu";
+            verbGrid[0, 2] = "El/Ella/Ud";
+            verbGrid[0, 3] = "Nosotros/as";
+            verbGrid[0, 4] = "Vosotros/as";
+            verbGrid[0, 5] = "Ellos/Ellas/Uds";
+            for (int i = 0; i < matches.Count; i++)
+            {
+                Regex rxRow = new Regex(@"<td[^>]*>([^<]+)</td>\s*");
+                var rowsMatches = rxRow.Matches(matches[i].Groups[0].Value);
+                Match[] matchesArray = new Match[rowsMatches.Count];
+                rowsMatches.CopyTo(matchesArray, 0);
+                List<Match> filteredRows = matchesArray.Where(m => !m.Value.Contains("english")).ToList();
+
+                for (int j = 0; j < filteredRows.Count; j++)
+                {
+                    verbGrid[i + 1, j] = filteredRows[j].Groups[1].Value;
+                }
+            }
+
+            return TableFromArray(matches.Count, matchesGerund[0].Groups[matchesGerund[0].Groups.Count - 1].Value, verbGrid);
+        }
+
+        private static string TableFromArray(int numberOfTenses, string gerund, string[,] verbGrid)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("<table border=\"1\" cellspacing=\"0\">");
+            for (int i = 0; i < 6; i++)
+            {
+                sb.AppendLine("<tr>");
+                for (int j = 0; j < verbGrid.Length / 6; j++)
+                {
+                    sb.Append(string.Format("\t<td>{0}</td>\r\n", verbGrid[j, i]));
+                }
+                sb.AppendLine("</tr>");
+            }
+            sb.Append(string.Format("<tr><td colspan='{0}'>", numberOfTenses + 1)).Append(gerund).AppendLine("</td><tr>");
+            sb.Append("</table>");
+            return sb.ToString();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.A)
+            {
+                textBox2.SelectAll();
+            }
+
+            if (e.Control && e.KeyCode == Keys.C)
+            {
+                textBox2.Copy();
+            }
+            if (e.Control && e.KeyCode == Keys.V)
+            {
+                //textBox2.Paste();
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.A)
+            {
+                textBox1.SelectAll();
+            }
+
+            if (e.Control && e.KeyCode == Keys.C)
+            {
+                textBox1.Copy();
+            }
+            if (e.Control && e.KeyCode == Keys.V)
+            {
+                //textBox1.Text = string.Empty;
+                //textBox1.Paste();
+                //return;
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = CallWebSite("http://www.123teachme.com/spanish_verb_conjugation/{0}");
+            if (string.IsNullOrEmpty(textBox1.Text))
+                return;
+
+            textBox2.Text = TableFrom123Spanish();
+            if (string.IsNullOrEmpty(textBox2.Text) || checkBox1.Checked)
+            {
+
+                textBox1.Text = CallWebSite("http://en.bab.la/conjugation/spanish/{0}");
+
+                if (string.IsNullOrEmpty(textBox1.Text))
+                    return;
+
+                textBox2.Text = TableFromBabLa(textBox1.Text);
+            }
+        }
+
+        private string CallWebSite(string url)
+        {
+            HttpWebRequest HttpWReq =
+            (HttpWebRequest)WebRequest.Create(string.Format(url, textBox3.Text));
+
+            HttpWebResponse HttpWResp = (HttpWebResponse)HttpWReq.GetResponse();
+            StreamReader sr = new StreamReader(HttpWResp.GetResponseStream());
+            string result = sr.ReadToEnd();
+            // Insert code that uses the response object.
+            HttpWResp.Close();
+            return result;
+        }
+    }
+}
