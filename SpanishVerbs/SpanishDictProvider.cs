364fea32c9ce524c8ea6a19af2140f416bcf03cd
Vladimir Ivanov <terrorv@gmail.com>
2015-04-16 08:56:57 +0300

    Add new provider - SpanishDict

---
+++ SpanishVerbs/SpanishDictProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpanishVerbs
+{
+    public class SpanishDictProvider :IConjugationProvider
+    {
+        public Verb GetConjugation(string rawData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
